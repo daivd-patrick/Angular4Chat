c9ff5448fa21a0a9acc35fc4aa14816ea06c3f79
tiffany-ymt <tiffanyymt70@gmail.com>
2017-05-25 02:26:10 -0500

    Initial commit - Boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyALRYixD74st8o20Xc_Q8Vaq9d_34VXSxc",
+  authDomain: "myangularchat-e4035.firebaseapp.com",
+  databaseURL: "https://myangularchat-e4035.firebaseio.com",
+  projectId: "myangularchat-e4035",
+  storageBucket: "myangularchat-e4035.appspot.com",
+  messagingSenderId: "437182128499"
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +24,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
