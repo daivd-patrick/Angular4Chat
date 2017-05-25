24232803c8b612b2df8235daf36c898d6318df91
tiffany-ymt <tiffanyymt70@gmail.com>
2017-05-25 03:16:14 -0500

    Added login with Facebook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  items: FirebaseListObservable<any>;
+  name: any;
+  msgVal: string = '';
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+
+    // limit to last 5 messages
+    this.items = af.list('/messages', {
+      query: {
+        limitToLast: 5
+      }
+    });
+
+    // get user authentication state
+    this.name = this.afAuth.authState;
+
+  }
+
+  // login from Facebook using a popup
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+  }
+
+  // logout 
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  chatSend(theirMessage: string) {
+      this.items.push({ message: theirMessage, name: this.name.facebook.displayName});
+      this.msgVal = '';
+  }
 }
